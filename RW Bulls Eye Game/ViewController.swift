635399890f1d8d1a44e61ed353a953cc2933a0e9
Arlo Erdaka <arlo.erdaka@gmail.com>
2019-05-31 14:43:58 +0700

    add button and pop up alert

--- RW Bulls Eye Game/ViewController.swift
+++ RW Bulls Eye Game/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Halo Dunia!", message: "Ini game Bulls Eye!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Keren!", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 
 
